0da66540b8a7512402e5b1e750aa45b32046a7e8
hmdesai89 <hmdesai89@gmail.com>
2016-02-24 13:29:47 +0000

    Making vpc mandatory in Address and SecurityGroup Apis

--- ec2api/api/cloud.py
+++ ec2api/api/cloud.py
@@ -92,8 +92,12 @@ class CloudController(object):
     def __str__(self):
         return 'CloudController'
 
+
+    #making domain ID mendatory
+    #def allocate_address(self, context, domain=None):
+
     @module_and_param_types(address, 'str255')
-    def allocate_address(self, context, domain=None):
+    def allocate_address(self, context, domain):
         """Acquires an Elastic IP address.
 
         Args:
@@ -230,10 +234,13 @@ class CloudController(object):
             A list of security groups.
         """
 
+    #Creating VPC id mandatory for Security Group
+    #In future if you want to revert change do vpc_id=None
+
     @module_and_param_types(security_group, 'security_group_str',
                             'security_group_str', 'vpc_id')
     def create_security_group(self, context, group_name,
-                              group_description, vpc_id=None):
+                              group_description, vpc_id):
         """Creates a security group.
 
         Args:
