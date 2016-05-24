0521038e598133b2eb3168f2c93d460c4479168c
mayank <mgoyal10@gmail.com>
2016-05-24 11:55:25 +0000

    modified error message for JNT-69

--- ec2api/exception.py
+++ ec2api/exception.py
@@ -325,7 +325,7 @@ class InvalidGroupDuplicate(EC2DuplicateException):
 
 class InvalidGroupReserved(EC2ReservedException):
     ec2_code = 'InvalidGroup.Duplicate'
-    msg_fmt = _("Security group '%(name)s' reserved.")
+    msg_fmt = _("This security group name '%(name)s' is reserved and cannot be used to create a new security group. Please refer VPC API Reference Guide for more details.")
 
 class RouteAlreadyExists(EC2DuplicateException):
     msg_fmt = _('The route identified by %(destination_cidr_block)s '
